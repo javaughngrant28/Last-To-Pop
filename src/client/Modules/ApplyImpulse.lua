e4d1986781c0898152cfb099dce64dc59a3bf321
Grant <javaughngrant28@gmail.com>
2025-06-17 18:39:38 -0700

    ApplyImpulse Module

---
+++ src/client/Modules/ApplyImpulse.lua
@@ -0,0 +1,17 @@
+local ApplyImpulse = {}
+
+function ApplyImpulse.Fire(targetModel: Model, diraction: Vector3, distance: number, duration: number?)
+    local rootPart = targetModel:FindFirstChild('HumanoidRootPart') or targetModel.PrimaryPart :: Part
+
+    task.defer(function()
+        local duration = duration or 0.4
+        local force = (diraction * distance) / duration +  Vector3.new(0,workspace.Gravity * duration * 0.5,0)
+
+        rootPart.Velocity = Vector3.zero
+        rootPart.AssemblyLinearVelocity = Vector3.zero
+      
+        rootPart:ApplyImpulse(force * rootPart.AssemblyMass)
+    end)
+end
+
+return ApplyImpulse
