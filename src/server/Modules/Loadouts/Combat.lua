e77ba4488c8e17bf8ab1a4d85d8bcf29b047b960
Grant <javaughngrant28@gmail.com>
2025-06-26 10:58:27 -0700

    Combat Loadout

---
+++ src/server/Modules/Loadouts/Combat.lua
@@ -0,0 +1,55 @@
+
+local MaidModule = require(game.ReplicatedStorage.Shared.Modules.Maid)
+local PlayerBalloon = require(script.Parent.PlayerBalloon)
+
+local Tools = game.ReplicatedStorage.Assets.Tools
+
+
+
+local Combat = {}
+Combat.__index = Combat
+
+Combat._MAID = nil
+Combat._PLAYER = nil
+Combat._PLAYER_BALLOON = nil
+
+
+
+function Combat.new(player: Player)
+    local self = setmetatable({}, Combat)
+    self:__Constructor(player)
+    return self
+end
+
+
+function Combat:__Constructor(player: Player)
+    self._MAID = MaidModule.new()
+    self._PLAYER = player
+
+    local Balloon = PlayerBalloon.new(player)
+    self._PLAYER_BALLOON = Balloon
+    self._MAID['Balloon'] = Balloon
+
+    self:CreateGun()
+end
+
+
+function Combat:CreateGun()
+    local toolName = 'Gun1'
+    local tool = Tools:FindFirstChild(toolName)
+    
+    local toolClone = tool:Clone()
+    self._MAID['Tool'] = toolClone
+
+    toolClone.Parent = self._PLAYER.Backpack
+end
+
+function Combat:Destroy()
+    self._MAID:Destroy()
+
+    for index, _ in pairs(self) do
+         self[index] = nil
+     end
+end
+
+return Combat
