08afe89ec8822a08a707856c2de4d729c771d2bd
Grant <javaughngrant28@gmail.com>
2025-06-26 06:12:31 -0700

    Fload Balloon

---
+++ src/client/Controllers/Balloon/Float.lua
@@ -0,0 +1,51 @@
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local MaidModule = require(game.ReplicatedStorage.Shared.Modules.Maid)
+
+local Maids = {} :: {
+	[Part]: MaidModule.Maid
+}
+
+
+local function MakeObjectFloat(hoverPart: Part, object: Part, maxDistance: number)
+	if Maids[object] then return end
+
+	local Maid = MaidModule.new()
+	Maids[object] = Maid
+
+	local attachment0 = Instance.new("Attachment")
+	Maid['a0'] = attachment0
+	attachment0.Name = "Attachment0"
+	attachment0.Parent = hoverPart
+
+	local attachment1 = Instance.new("Attachment")
+	Maid['a1'] = attachment1
+	attachment1.Name = "Attachment1"
+	attachment1.Parent = object
+
+	local alignPosition = Instance.new("AlignPosition")
+	Maid['AlignPosition'] = alignPosition
+	alignPosition.Attachment0 = attachment1
+	alignPosition.Attachment1 = attachment0
+	alignPosition.MaxForce = math.huge
+	alignPosition.Responsiveness = 2
+	alignPosition.ApplyAtCenterOfMass = true
+	alignPosition.Parent = object
+
+	Maid:GiveTask(object.AncestryChanged:Connect(function(_, parent)
+		if not parent then
+			Maid[object]:Destroy()
+		end
+	end))
+
+	Maid:GiveTask(hoverPart.AncestryChanged:Connect(function(_, parent)
+		if not parent then
+			Maid[object]:Destroy()
+		end
+	end))
+end
+
+
+return {
+	Fire = MakeObjectFloat
+}
--- src/client/Controllers/Balloon/LocalBalloon.client.lua
+++ src/client/Controllers/Balloon/LocalBalloon.client.lua
@@ -1,37 +1,44 @@
 local Players = game:GetService("Players")
 
+local RemoteUtil = require(game.ReplicatedStorage.Shared.Utils.RemoteUtil)
+local MaidModule = require(game.ReplicatedStorage.Shared.Modules.Maid)
+local CharacterEvents = require(game.ReplicatedStorage.Shared.Modules.CharacterEvents)
+local Float = require(script.Parent.Float)
+
+local Maid: MaidModule.Maid = MaidModule.new()
 local player = Players.LocalPlayer
-
-local Balloons = workspace:WaitForChild('Balloons',10)
-local TRANSPARENCY = 0.4
+local TRANSPARENCY = 0.8
 
 
-local function BalloonFound(balloon: Model)
+local function balloonInstance(instance: Instance)
+
+    if instance:IsA('Beam') then
+        local beam = instance :: Beam
+        beam.Transparency = NumberSequence.new(TRANSPARENCY)
+    end
+
+    if not instance:IsA('BasePart') then return end
+    local part = instance :: BasePart
+    part.CanCollide = false
+    part.CanTouch = false
+    part.CanQuery = false
+
+    if part.Transparency >= TRANSPARENCY then return end
+    part.Transparency = TRANSPARENCY
+end
+
+local function BalloonGiven(balloon: Model)
+    Maid['ChildAdded'] = balloon.DescendantAdded:Connect(balloonInstance)
     for _, instance: Instance in balloon:GetDescendants() do
-        
-        if not instance:IsA('BasePart') then continue end
-        instance.CanCollide = false
-        instance.CanTouch = false
-        instance.CanQuery = false
-
-        if instance.Transparency >= TRANSPARENCY then continue end
-        instance.Transparency = TRANSPARENCY
+        balloonInstance(instance)
     end
+
+    local head = player.Character:WaitForChild('Head',10)
+    Float.Fire(head,balloon.PrimaryPart,4)
 end
 
 
-local function BallonAdded(Balloon: Model)
-    if Balloon:IsA('Model') and Balloon.Name == player.Name then
-        BalloonFound(Balloon)
-    end
-end
-
-
-Balloons.ChildAdded:Connect(BallonAdded)
-
-for _, balloon: Model in Balloons:GetChildren() do
-    BallonAdded(balloon)    
-end
+RemoteUtil.OnClient('Balloon',BalloonGiven)
 
 
 
