d443c2c343fbcc8f6887db5d83064e7868f5a63f
Grant <javaughngrant28@gmail.com>
2025-06-26 06:20:10 -0700

    Removed Daid State Upate

--- src/client/Controllers/Character/CharacterState.client.lua
+++ src/client/Controllers/Character/CharacterState.client.lua
@@ -2,32 +2,16 @@ local Players = game:GetService('Players')
 local LocalPlayer = Players.LocalPlayer
 
 
-local function enableCustomHumanoidStates(character: Model)
-	local humanoid = character:WaitForChild('Humanoid', 30)
+local function enableCustomHumanoidStates(character: Model, humanoid: Humanoid)
 	humanoid:SetStateEnabled(Enum.HumanoidStateType.Ragdoll, false)
 	humanoid:SetStateEnabled(Enum.HumanoidStateType.FallingDown, false)
 	humanoid:SetStateEnabled(Enum.HumanoidStateType.Swimming, false)
 end
 
-local function onCharacterDied()
-	local character = LocalPlayer.Character
-	local humanoid = character:FindFirstChild('Humanoid') :: Humanoid
-	humanoid:SetStateEnabled(Enum.HumanoidStateType.Ragdoll, true)
-	humanoid:SetStateEnabled(Enum.HumanoidStateType.FallingDown, true)
-
-	task.wait(1)
-
-	humanoid:ChangeState(Enum.HumanoidStateType.Ragdoll)
-	humanoid:ChangeState(Enum.HumanoidStateType.FallingDown)
-end
-
 local function onCharacterAdded(character: Model)
 	if not character then return end
-
 	local humanoid = character:WaitForChild('Humanoid') :: Humanoid
-
-	-- humanoid.Died:Once(onCharacterDied)
-	enableCustomHumanoidStates(character)
+	enableCustomHumanoidStates(character,humanoid)
 end
 
 
